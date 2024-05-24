7913adadb4fb3615d51f752a8d8a2c48452e757d
isaacMwesigwa <mwesigwai433@gmail.com>
2024-05-24 09:38:14 +0300

    restored xml comments in swagger

--- Hubtel.UserWallet.Api/Program.cs
+++ Hubtel.UserWallet.Api/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +16,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+var xmlPath = Path.Combine(AppContext.BaseDirectory,xmlFile);
+builder.Services.AddSwaggerGen(c=>
+{
+    c.IncludeXmlComments(xmlPath);
+});
 builder.Services.AddScoped<IWalletService, WalletService>();
 builder.Services.AddSingleton<IHelperMethods, HelperMethods>();
 builder.Services.AddSingleton<IServiceResponseFactory<IWalletServiceResponse>, ServiceResponseFactory>();
